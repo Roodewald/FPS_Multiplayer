52326737ff1887cfcdf68f5dc4a66c00055214ee
Roodewald SP4 <Pavel999977@gmail.com>
2021-03-02 11:13:14 +0500

    no message

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using TMPro;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
+    [SerializeField] TMP_InputField roomNameInputField;
+    [SerializeField] TMP_Text errorText;
+
     void Start()
     {
         Debug.Log("Connecting to Master");
@@ -19,11 +23,28 @@ public class Launcher : MonoBehaviourPunCallbacks
 
     public override void OnJoinedLobby()
     {
+        MenuManager.instanse.OpenMenu("title");
         Debug.Log("Lobby joined");
     }
 
     void Update()
     {
-        
+        if (string.IsNullOrEmpty(roomNameInputField.text))
+        {
+            return;
+        }
+        PhotonNetwork.CreateRoom(roomNameInputField.text);
+        MenuManager.instanse.OpenMenu("loading");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        MenuManager.instanse.OpenMenu("room");
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        errorText.text = "Room Creation Failed: " + message;
+        MenuManager.instanse.OpenMenu("error");
     }
 }
