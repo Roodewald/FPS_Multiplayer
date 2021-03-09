a2244a8bc1b117805b46177d728f943a2be9dfa7
Roodewald SP4 <Pavel999977@gmail.com>
2021-03-09 10:25:40 +0500

    Заморозил проэкт

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using TMPro;
+using Photon.Realtime;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
     [SerializeField] TMP_InputField roomNameInputField;
     [SerializeField] TMP_Text errorText;
+    [SerializeField] TMP_Text roomNameText;
+    [SerializeField] Transform roomListContent;
+    [SerializeField] GameObject roomListItemPrefab;
 
     void Start()
     {
@@ -27,7 +31,7 @@ public class Launcher : MonoBehaviourPunCallbacks
         Debug.Log("Lobby joined");
     }
 
-    void Update()
+    public void CreateRoom()
     {
         if (string.IsNullOrEmpty(roomNameInputField.text))
         {
@@ -40,11 +44,24 @@ public class Launcher : MonoBehaviourPunCallbacks
     public override void OnJoinedRoom()
     {
         MenuManager.instanse.OpenMenu("room");
+        roomNameText.text = PhotonNetwork.CurrentRoom.Name;
     }
 
     public override void OnCreateRoomFailed(short returnCode, string message)
     {
         errorText.text = "Room Creation Failed: " + message;
         MenuManager.instanse.OpenMenu("error");
+    }
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+        MenuManager.instanse.OpenMenu("loading");
+    }
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        for (int i = 0; i < roomList.Count; i++)
+        {
+            Instantiate(roomListItemPrefab, roomListContent).GetComponent<RoomListItem>().SetUp(roomList[i]);
+        }
     }
 }
---
+++ Assets/Scripts/RoomListItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Realtime;
+using TMPro;
+
+public class RoomListItem : MonoBehaviour
+{
+
+    [SerializeField] TMP_Text text;
+
+    
+
+    public void SetUp(RoomInfo _info)
+    {
+        
+        text.text = _info.Name;
+    }
+
+    public void OnClick()
+    {
+
+    }
+}
